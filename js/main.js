15cf68fc9be13ec5c82928bbc39d0e4fec71da62
moonstarsky37 <moon.starsky37@gmail.com>
2016-04-21 16:15:44 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var API_PATH = "http://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AV&CaseNo2=1&FileType=1&Lang=C&FolderType="
-
+//var API_PATH = "http://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AV&CaseNo2=1&FileType=1&Lang=C&FolderType="
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=6f4e0b9b-8cb1-4b1d-a5c4-febd90f62469";
 function getData(callback) {
     $.ajax({
             url : API_PATH,
